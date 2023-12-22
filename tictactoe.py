98e66c5756c314fdd44aa1d420fe8b920059fa5b
christiantyemele <yemelechristian2@gmail.com>
2023-12-22 13:47:47 +0100

    fix(tictactoe): making the tictatoe functional again

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,27 @@
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-            'low-L': ' ', 'low-M': ' ', 'low-R': ' ' }
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+
 def printboard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] + ' ' )
-    print(board['mid-L'] + '|' + board['top-M'] + '|' + board['top-R'] + ' ')
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] + ' ')
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'] + ' ')
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'] + ' ')
+
+
+Turn = 'X'
+
+for i in range(9):
+    printboard(theBoard)
+    print('Turn for ' + Turn + ' to move')
+    move = input()
+    theBoard[move] = Turn
+    if Turn == 'X':
+        Turn = 'O'
+
+    else:
+        Turn = 'X'
+
 print(printboard(theBoard))
